bd2d2a7ea7f5e1e959a7e920cefedcddc81aac1e
WelcomeThrillhoTDC <37635490+WelcomeThrillhoTDC@users.noreply.github.com>
2026-04-09 15:33:13 -0400

    Update leaderboard.js

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,6 +1,7 @@
 const SPREADSHEET_ID = "1BUaE0fYx0trpWqPQkQpeGWYmAAJvUvcZKDS78HjE7N8";
 
 const seasons = [
+  { label: "Teenage Mutant Ninja Turtles", tab: "Teenage Mutant Ninja Turtles" },
   { label: "Lorwyn Eclipsed", tab: "Lorwyn Eclipsed" },
   { label: "Avatar: The Last Airbender", tab: "Avatar: The Last Airbender" },
   { label: "Marvel's Spider-Man", tab: "Marvel's Spider-Man" },
